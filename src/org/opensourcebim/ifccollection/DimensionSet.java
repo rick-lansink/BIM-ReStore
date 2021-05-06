6d59500e950f45e5a1aa9c52f6db15f496e82aed
lansink.rick@gmail.com <rick.lansink@jstack.eu>
2021-05-06 15:24:21 +0200

    Added surface area and volume to dimension sets

--- src/org/opensourcebim/ifccollection/DimensionSet.java
+++ src/org/opensourcebim/ifccollection/DimensionSet.java
@@ -17,7 +17,8 @@ public class DimensionSet {
 	}
 
 	private String dimensionSetHash;
-	
+	private double totalSurfaceArea;
+	private double totalVolume;
 	private List<String> children;
 	
 	public String getDimensionHash() {
@@ -28,11 +29,31 @@ public class DimensionSet {
 		this.dimensionSetHash = hash;
 	}
 	
+	public double getTotalSurfaceArea() {
+		return totalSurfaceArea;
+	}
+	
+	public double getTotalVolume() {
+		return totalVolume;
+	}
+	
 	public List<String> getUsedByObjects() {
 		return children;
 	}
 	
 	private void setUsedByObjects(List<ReStoreObject> usedByObjects) {
 		this.children = usedByObjects.stream().map(o -> o.getGlobalId()).collect(Collectors.toList());
+		this.setTotalSurfaceArea(usedByObjects);
+		this.setTotalVolume(usedByObjects);
+	}
+	
+	private void setTotalSurfaceArea(List<ReStoreObject> usedByObjects) {
+		this.totalSurfaceArea = usedByObjects.stream().map(o -> o.getGeometry().getLargestFaceArea())
+					.collect(Collectors.summingDouble(d -> d));
+	}
+	
+	private void setTotalVolume(List<ReStoreObject> usedByObjects) {
+		this.totalVolume = usedByObjects.stream().map(o -> o.getGeometry().getVolume())
+					.collect(Collectors.summingDouble(d -> d));
 	}
 }
